4b3ecb28e27840066ec5999e26f4e430ebda39db
rohittrana <rohitrana2429@gmail.com>
2025-02-04 22:29:25 +0530

    filter

---
+++ lecture-28/index.js
@@ -0,0 +1,9 @@
+const coding = ["js","cpp","java","python","c"];
+ const values = coding.forEach((item)=>{
+               console.log(item);
+           
+ })
+
+const newNums = myNums.filter((num)=>{
+               
+})
