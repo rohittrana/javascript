5a48ff10652c009a4bbc5289ac165efc5b541b39
rohittrana <rohitrana2429@gmail.com>
2025-02-25 21:07:10 +0530

    dom

--- lecture-28/four.js
+++ lecture-28/four.js
@@ -1,7 +1,28 @@
-const array1 =[100 , 200, 300,400,500];
+const array1 = [100, 200, 300, 400, 500];
 
-
-const myTotal = array1.reduce(function (acc,curr){
-               return acc+curr
-},0);
+const myTotal = array1.reduce((acc, curr) => {
+  return acc + curr;
+}, 0);
 console.log(myTotal);
+
+const obj = [
+  {
+    Item: "js cours",
+    price: 200,
+  },
+  {
+    Item: "js cours",
+    price: 400,
+  },
+  {
+    Item: "js cours",
+    price: 1000,
+  },
+  {
+    Item: "js cours",
+    price: 400,
+  },
+];
+
+ const totalprice =obj.reduce((acc,item)=>item.price+acc,0);
+console.log(totalprice);
--- lecture-28/index.js
+++ lecture-28/index.js
@@ -1,21 +1,18 @@
-const coding = ["js","cpp","java","python","c"];
- const values = coding.forEach((item)=>{
-               console.log(item);
-           
- })
+const coding = ["js", "cpp", "java", "python", "c"];
+const values = coding.forEach((item) => {
+  console.log(item);
+});
 
 // const newNums = myNums.filter((num)=>{
-// return num >4;               
+// return num >4;
 // })
 
-const myNums = [1,3,4,5,6,7,76,8,98,56,43];
+const myNums = [1, 3, 4, 5, 6, 7, 76, 8, 98, 56, 43];
 const newNums = [];
 
-myNums.forEach((num)=>{
-               if(num>4){
-                       newNums.push(num)
-               }
-               
-})
+myNums.forEach((num) => {
+  if (num > 4) {
+    newNums.push(num);
+  }
+});
 console.log(newNums);
-
